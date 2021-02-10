7650b34dfce3ae42fd12b099eabd55e09989500f
Joseph Weissman <joseph@bidops.com>
2021-02-10 11:25:05 -0500

    start refactoring eve test

--- src/eve/Eve.test.ts
+++ src/eve/Eve.test.ts
@@ -4,76 +4,68 @@ import { EveInteger } from './vm/data-types/EveInteger'
 import { Opcode } from './vm/Opcode'
 import { RegistryKey } from './vm/RegistryKey'
 import { inst, label, goto, call } from './vm/InstructionHelpers'
+import { Instruction } from './vm/types'
+
+const noop = inst(Opcode.NOOP)
+const lconst_one = inst(Opcode.LCONST_ONE)
+const lconst_two = inst(Opcode.LCONST_TWO)
+const lconst_zero = inst(Opcode.LCONST_ZERO)
+const lconst_idx = (index: number) => inst(Opcode.LCONST_IDX, index)
+const iadd = inst(Opcode.INT_ADD)
+const imul = inst(Opcode.INT_MUL)
+const str_join = inst(Opcode.STR_JOIN)
+const pop2 = inst(Opcode.POP2)
+const ret = inst(Opcode.RET)
+
+let eve: Eve
+const expectEve = (...instructions: Instruction[]) => {
+  const result = eve.execute(instructions)
+  return expect(result.js)
+}
 
 describe(Eve, () => {
-  let eve: Eve
   beforeEach(() => { eve = new Eve() })
 
-  it('noop', () => {
-    const result = eve.execute([ inst(Opcode.NOOP) ])
-    expect(result.js).toEqual(null)
+  it('noop', () => expectEve(noop).toEqual(null))
+  it('sums', () => {
+    expectEve(lconst_one, lconst_one, iadd).toEqual(2)
+    expectEve(lconst_one, lconst_two, iadd).toEqual(3)
+    expectEve(lconst_two, lconst_two, iadd).toEqual(4)
+    expectEve(lconst_one, lconst_zero, iadd).toEqual(1)
+    expectEve(lconst_zero, lconst_zero, iadd).toEqual(0)
   })
-
-  it('computes the sum of one added to itself', () => {
-    const result = eve.execute([
-      inst(Opcode.LCONST_ONE),
-      inst(Opcode.LCONST_ONE),
-      inst(Opcode.INT_ADD)
-    ])
-    expect(result.js).toEqual(2)
-  })
-  
-  it('computes the sum of one and two', () => {
-    const result = eve.execute([
-      inst(Opcode.LCONST_ONE),
-      inst(Opcode.LCONST_TWO),
-      inst(Opcode.INT_ADD)
-    ])
-    expect(result.js).toEqual(3)
-  })
-
-  it('computes the sum of zero and one', () => {
-    const result = eve.execute([
-      inst(Opcode.LCONST_ONE),
-      inst(Opcode.LCONST_ZERO),
-      inst(Opcode.INT_ADD)
-    ])
-    expect(result.js).toEqual(1)
+  it('products', () => {
+    expectEve(lconst_one, lconst_one,  imul).toEqual(1)
+    expectEve(lconst_one, lconst_two,  imul).toEqual(2)
+    expectEve(lconst_two, lconst_two,  imul).toEqual(4)
+    expectEve(lconst_one, lconst_zero, imul).toEqual(0)
+    expectEve(lconst_zero, lconst_zero, imul).toEqual(0)
   })
   
-  it('computes the sum of larger numbers', () => {
+  it('math on constants from pool', () => {
     eve.vm.constantPool = [ new EveInteger(3), new EveInteger(4) ]
-    const result = eve.execute([
-      inst(Opcode.LCONST_IDX, 0),
-      inst(Opcode.LCONST_IDX, 1),
-      inst(Opcode.INT_ADD)
-    ])
-    expect(result.js).toEqual(7)
+    expectEve(lconst_idx(0), lconst_idx(1), iadd).toEqual(7)
+    eve.vm.constantPool = [ new EveInteger(10), new EveInteger(20) ]
+    expectEve(lconst_idx(0), lconst_idx(1), imul).toEqual(200)
   })
 
   it('joins strings', () => {
     eve.vm.constantPool = [ new EveString('hello '), new EveString('world') ]
-    const result = eve.execute([
-      inst(Opcode.LCONST_IDX, 0),
-      inst(Opcode.LCONST_IDX, 1),
-      inst(Opcode.STR_JOIN)
-    ])
-    expect(result.js).toEqual('hello world')
+    expectEve(lconst_idx(0), lconst_idx(1), str_join).toEqual('hello world')
   })
 
   it('stores and loads values', () => {
     eve.vm.constantPool = [ new EveString('hello '), new EveString('there') ]
-    const result = eve.execute([
-      inst(Opcode.LCONST_IDX, 0),
+    expectEve(
+      lconst_idx(0),
       inst(Opcode.ASTORE, RegistryKey.A),
-      inst(Opcode.LCONST_IDX, 1),
+      lconst_idx(1),
       inst(Opcode.ASTORE, RegistryKey.B),
-      inst(Opcode.POP2),
+      pop2,
       inst(Opcode.LSTORE, RegistryKey.A),
       inst(Opcode.LSTORE, RegistryKey.B),
-      inst(Opcode.STR_JOIN),
-    ])
-    expect(result.js).toEqual('hello there')
+      str_join,
+    ).toEqual('hello there')
   })
 
   describe('throw', () => {
@@ -93,15 +85,15 @@ describe(Eve, () => {
   })
 
   it('jumps to a specific program offset', () => {
-    const result = eve.execute([
-      inst(Opcode.LCONST_ONE),  // 0
+    // const result = eve.execute([
+    expectEve(
+      lconst_one,
       inst(Opcode.JUMP_Z, 4),   // 1
-      inst(Opcode.LCONST_ZERO), // 2
+      lconst_zero,
       inst(Opcode.JUMP_Z, 5),   // 3
       inst(Opcode.THROW),       // 4
-      inst(Opcode.LCONST_TWO),  // 5
-    ])
-    expect(result.js).toEqual(2)
+      lconst_two,
+    ).toEqual(2)
   })
 
   it('goto label', () => {
@@ -110,7 +102,7 @@ describe(Eve, () => {
       new EveInteger(10)
     ]
     const result = eve.execute([
-      inst(Opcode.LCONST_IDX, 1), 
+      lconst_idx(1),
       inst(Opcode.ASTORE, RegistryKey.A),
       label('start'),
       inst(Opcode.LSTORE, RegistryKey.A),
@@ -119,8 +111,8 @@ describe(Eve, () => {
       goto('done'),
       label('sub'),
       inst(Opcode.LSTORE, RegistryKey.A),
-      inst(Opcode.LCONST_IDX, 0), 
-      inst(Opcode.INT_ADD),
+      lconst_idx(0),
+      iadd,
       inst(Opcode.ASTORE, RegistryKey.A),
       goto('start'),
       label('done'),
@@ -144,7 +136,7 @@ describe(Eve, () => {
       goto('done'),
       inst(Opcode.THROW),
       label('sub'),
-      inst(Opcode.LCONST_IDX, 0),
+      lconst_idx(0),
       inst(Opcode.RET),
       inst(Opcode.THROW), // execution should not get here :)
       label('done'),
@@ -154,21 +146,20 @@ describe(Eve, () => {
 
   it('invokes a unary subroutine and returns', () => {
     eve.vm.constantPool = [ new EveInteger(2), new EveInteger(4) ]
-    const result = eve.execute([
+    expectEve(
       label('main'),
-      inst(Opcode.LCONST_IDX, 1),
+      lconst_idx(1),
       call('double', 1),
       call('double', 1),
       goto('done'),
       inst(Opcode.THROW),
       label('double'),
-      inst(Opcode.LCONST_IDX, 0),
-      inst(Opcode.INT_MUL),
-      inst(Opcode.RET),
+      lconst_idx(0),
+      imul,
+      ret,
       inst(Opcode.THROW),
       label('done')
-    ])
-    expect(result.js).toEqual(16)
+    ).toEqual(16)
   })
 
   // call a method by reference...
